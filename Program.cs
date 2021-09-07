acbdb4c4c71fb657c6603a239ed6d493b855d94b
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-06 22:23:10 -0300

    finaliza programa Criança Eduacada

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CriancaEducada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue; 
+            
+            Console.WriteLine("olá usuário, eu duvido você tentar me xingar HAHAHA");
+            String CantSay = Console.ReadLine()
+            .Replace("chata", "@!$#@$!#")
+            .Replace("chato", "@#!$#@")
+            .Replace("boba", "@#!$#@")
+            .Replace("bobo", "@#!$#@")
+            .Replace("feio", "@!$#@$!#")
+            .Replace("feia", "@#!$#@")
+            .Replace("boboca", "@#!$#@")
+            .Replace("bocó", "@#!$#@")
+            .Replace("tonto", "@#!$#@")
+            .Replace("tonta", "@!$#@$!#")
+            .Replace("palerma", "@#!$#@")
+            .Replace("paspalho", "@#!$#@")
+            .Replace("paspalha", "@#!$#@")
+            .Replace("tantã", "@#!$#@")
+            .Replace("panaca", "@!$#@$!#")
+            .Replace("pentelho", "@#!$#@")
+            .Replace("pentelha", "@#!$#@")
+            .Replace("burro", "@#!$#@")
+            .Replace("burra", "@#!@#!#")
+            .Replace("besta", "@!#!#!@")
+            ;
+            
+
+            Console.WriteLine($"{CantSay}");
+
+            
+
+
+
+
+
+
+            
+        }
+    }
+}
